f7940d341cbd96f7c2bc39014d6dcda89bec6298
nashmaniac <nishantve1@gmail.com>
2015-05-19 17:23:49 +0530

    Started working on Binary Search Tree

---
+++ Binary Search Tree/src/BST.java
@@ -0,0 +1,9 @@
+/*
+ * Created by Nash on 19/05/15.
+ *
+ * File : BST.java/(Binary Search Tree)
+ * -------------------------------------
+ */
+public class BST {
+    
+}
---
+++ Binary Search Tree/src/Client.java
@@ -0,0 +1,8 @@
+/*
+* Author : Nash Vail
+*/
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
