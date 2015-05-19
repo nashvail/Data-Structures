89d99939994d84ddc9c5c4e54374d721f7ff6dbd
nashmaniac <nishantve1@gmail.com>
2015-05-19 21:03:49 +0530

    Code improvements and added gitgnore

--- Binary Search Tree/src/BST.java
+++ Binary Search Tree/src/BST.java
@@ -19,28 +19,37 @@ public class BST<Integer> {
     }
 
     public void addNode(Node<Integer> newNode) {
-        if(numNodes == 0)
+        if(numNodes == 0) {
             root = newNode;
-        else
-           insertNode(root, newNode);
+            numNodes++;
+        } else {
+            insertNode(root, newNode);
+            numNodes++;
+        }
+    }
+
+    public Node<Integer> getRootNode() {
+        return root;
     }
 
 
+    /*
+    * Function : insertNode(Parent under consideration, Node to be inserted)
+    * ----------------------------------------------------------------------
+    * Recursively inserts a new node at its proper place in the tree.
+    */
     public void insertNode(Node<Integer> currentParent, Node<Integer> newNode) {
         if (newNode.getNodeData() < currentParent.getNodeData()) {
-            if(currentParent.getLeftChild() == null) {
+            if(currentParent.getLeftChild() == null)
                 currentParent.setLeftChild(newNode);
-                return;
-            } else {
+            else
                 insertNode(currentParent.getLeftChild(), newNode);
-            }
+
         } else {
-            if(currentParent.getRightChild() == null) {
+            if(currentParent.getRightChild() == null)
                 currentParent.setRightChild(newNode);
-                return;
-            } else {
+            else
                 insertNode(currentParent.getRightChild(), newNode);
-            }
         }
     }
 
--- Binary Search Tree/src/Client.java
+++ Binary Search Tree/src/Client.java
@@ -3,6 +3,12 @@
 */
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BST<Integer> tree = new BST<Integer>();
+        tree.addNode(new Node<Integer>(4));
+        tree.addNode(new Node<Integer>(3));
+        tree.addNode(new Node<Integer>(5));
+        System.out.println("Root " + tree.getRootNode().getNodeData());
+        System.out.println("Left Child " + tree.getRootNode().getLeftChild().getNodeData());
+        System.out.println("Right Child " + tree.getRootNode().getRightChild().getNodeData());
     }
 }
